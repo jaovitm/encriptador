31774460af96bb0c0bd0eb396bd5bd83475fe263
jaovitm <103443109+jaovitm@users.noreply.github.com>
2022-04-19 14:08:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,32 +9,56 @@ As "chaves" de criptografia que utilizaremos são:
 
 const txt = document.getElementById("texto");
 const dcr = document.getElementById("resposta");
+const his = document.getElementById("his");
 var cr;
-var respostas = [];
-const resposta = resposta =>{
-    respostas.push(resposta)
-    dcr.value = respostas
+var count = 1
+var i = 0
+var acc = "";
+var historico = [];
+
+const constroi = () =>{
+    acc+= `
+    <h3>${count}ª solicitação:<span class="action"> &nbsp &nbsp ${historico[i]}</span></h3>
+    `
+}
+
+const resultado = (resposta) =>{
+    dcr.value = "";
+    dcr.value = resposta
+    historico.push(resposta)
+    constroi()
+    his.innerHTML = acc 
+    count++;
+    i++;
 }
 const criptografar = () =>{
-    txt.value == ""? alert("Você não pode criptofrafar o nada!") :
-    cr = txt.value.replace(/i/g, "imes");
+    txt.value == ""? alert("Você não pode criptografar o nada!") :
+    cr = txt.value.toLowerCase().replace(/i/g, "imes");
     cr = cr.replace(/e/g, "enter");
     cr = cr.replace(/a/g, "ai");
     cr = cr.replace(/o/g, "ober");
     cr = cr.replace(/u/g, "ufat");
-    resposta(cr)
+    resultado(cr);
 }
 const descriptografar = () =>{
-    txt.value == ""? alert("Você não pode descriptofrafar o nada!") :
-    cr = txt.value.replace(/ai/g, "a");
+    txt.value == ""? alert("Você não pode descriptografar o nada!") :
+    cr = txt.value.toLowerCase().replace(/ai/g, "a");
     cr = cr.replace(/enter/g, "e");
     cr = cr.replace(/imes/g, "i");
     cr = cr.replace(/ober/g, "o");
     cr = cr.replace(/ufat/g, "u");
-    resposta(cr)
+    resultado(cr)
 }
 const copy = () => {
-    i = respostas.length
-    navigator.clipboard.writeText(respostas[i-1]);
+    i = historico.length
+    navigator.clipboard.writeText(historico[i-1]);
+}
+const history = () =>{
+    var modal = document.getElementById("modal");
+    modal.classList.add("show-modal");
+}
+const closeHistory = () =>{
+    var modal = document.getElementById("modal");
+    modal.classList.remove("show-modal");
 }
 
